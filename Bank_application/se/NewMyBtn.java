b3a4b500fdbd8c376765cf7b0b0992b73b164042
DongGu Kang <ehdrnsla3020@naver.com>
2017-03-07 15:19:18 +0900

    Update NewMyBtn.java

--- Bank_application/se/NewMyBtn.java
+++ Bank_application/se/NewMyBtn.java
@@ -13,7 +13,7 @@ public class NewMyBtn {
 	void myBtn(JButton Btn){
 		Btn.setBackground(Color.LIGHT_GRAY);
 		Btn.setBorder(new BevelBorder(BevelBorder.RAISED));
-		Btn.setForeground(Color.BLACK); //¿ä°Ç ±Û¾¾»ö
+		Btn.setForeground(Color.BLACK); 
 		
 	}
 }
